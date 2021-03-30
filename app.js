dca5417cf5d279a4ceb01dbf29b8b3471f40bbce
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2021-03-30 05:39:55 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "rgb(255, 0, 255)", "#800080", "#00FF00"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
